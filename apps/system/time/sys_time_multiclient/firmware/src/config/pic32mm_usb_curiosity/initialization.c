f36ad9e897abb68204feebac950cdcb73fa898b2
Arpan Kumar <arpan.kumar@microchip.com>
2021-08-17 16:04:16 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_pic32mm_usb_curiosity
    MH3-55683
    
    Signed-off-by: Arpan Kumar <arpan.kumar@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32mm_usb_curiosity/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mm_usb_curiosity/initialization.c
@@ -80,7 +80,7 @@
 #pragma config FWDTEN =     OFF
 
 /*** FOSCSEL ***/
-#pragma config FNOSC =      PLL
+#pragma config FNOSC =      FRCDIV
 #pragma config PLLSRC =     FRC
 #pragma config SOSCEN =    OFF
 #pragma config IESO =       ON
